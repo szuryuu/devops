0be723e6198efb389fe93342888c3d07732b7d7f
shfwnz <ilhamdzaky2007@gmail.com>
2025-08-05 21:01:08 +0700

    fix: sending to requestbin

--- linux-admin/scripts/disk-monitor.sh
+++ linux-admin/scripts/disk-monitor.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo $(df -h /)
+URL="https://eo5wtn983pri732.m.pipedream.net"
+
+count=$(df -h | awk 'NR>1 && int($5) >= 20' | wc -l)
+
+echo "Total high usage disks: $count"
+
+df -h | awk 'NR>1 && int($5) >= 20' | while read -r FS SIZE USED UNUSED PERCENT MOUNT
+do
+    curl -X POST $URL \
+    -H "Content-Type: application/json" \
+    -d '{
+        "Total Size": "'$SIZE'",
+        "Used Size": "'$USED' '$PERCENT'",
+        "Free Size": "'$UNUSED'",
+        "Mounted on": "'$MOUNT'"
+    }'
+
+    echo "$SIZE, $USED, $UNUSED, $PERCENT, $MOUNT"
+done
