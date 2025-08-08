f270dd61bb35c8dfe7551e845ea97606a365ac87
shfwnz <ilhamdzaky2007@gmail.com>
2025-08-08 10:46:56 +0700

    fix: create user bash

--- linux-admin/scripts/create-user.sh
+++ linux-admin/scripts/create-user.sh
@@ -2,12 +2,21 @@
 
 URL="https://eo5wtn983pri732.m.pipedream.net"
 
-groupadd testusers
+if ! getent group testusers &>/dev/null; then
+    groupadd testusers
+fi
+
 for i in {1001..1100}; do
     USERNAME="user-$i"
 
+    if id $USERNAME &>/dev/null; then
+        echo "User $USERNAME already exists"
+        continue
+    fi
+
     useradd -u $i -g testusers $USERNAME
     GROUPNAME=$(id -g -n $USERNAME)
+    GID=$(id -g "$USERNAME")
 
     curl -X POST $URL \
     -H "Content-Type: application/json" \
@@ -15,10 +24,9 @@ for i in {1001..1100}; do
         "username": "'$USERNAME'",
         "uid": '$i',
         "group": "'$GROUPNAME'",
-        "gid": '$(getent group testusers | cut -d: -f3)'
+        "gid": '$GID'
     }'
 
     echo "User $USERNAME created"
-    echo $GROUPNAME
     sleep 0.05
 done
