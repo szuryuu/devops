ba72bd39ab60c4acc8d3dfd86ee20354aef18f17
shfwnz <ilhamdzaky2007@gmail.com>
2025-08-10 21:55:39 +0700

    fix: nginx not working

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-echo "APP_UID=${APP_UID}"
-echo "APP_GID=${APP_GID}"
-
 if [ ! -f /etc/nginx/ssl/certs/server.crt ]; then
     echo "SSL certificate not found, generating self-signed cert..."
     bash ./ssl/generate-ssl.sh
 fi
 
 nginx -t
-
-if [ $? -eq 0 ]; then
-    echo "Nginx configuration is valid"
-    nginx -g 'daemon off;'
-else
+if [ $? -ne 0 ]; then
     echo "Nginx configuration error"
     exit 1
 fi
+
+exec nginx -g 'daemon off;'
