bc27e652f6a6d3f4d7f9b22730cef7a67a62159f
shfwnz <ilhamdzaky2007@gmail.com>
2025-08-11 07:53:37 +0700

    fix: nginx running custom users

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Running as user: $(id)"
+
 if [ ! -f /etc/nginx/ssl/certs/server.crt ]; then
     echo "SSL certificate not found, generating self-signed cert..."
     bash ./ssl/generate-ssl.sh
@@ -10,5 +12,8 @@ if [ $? -ne 0 ]; then
     echo "Nginx configuration error"
     exit 1
 fi
+
+echo $(ls -l /run/nginx.pid)
+echo $(ls -ld /run)
 
 exec nginx -g 'daemon off;'
